c0df887ba127f2248b3e690e1f08aabed00bbf60
Thais-Faria <tf_bio@proton.me>
2025-04-18 20:13:22 -0300

    Modificação para deixar o nome das colunas conforme o original

--- scripts/pbdb_occurrence_names.R
+++ scripts/pbdb_occurrence_names.R
@@ -4,14 +4,11 @@
 # e anos.
 
 # Carregando pacotes e dados
-
+library(here)
 library(groundhog)
 groundhog.day <- "2025-04-01"
 groundhog.packages <- c("here",
-                        "tidyr",
                         "dplyr",
-                        "stringr",
-                        "forcats",
                         "tidylog")
 
 groundhog.library(groundhog.packages,
@@ -23,32 +20,9 @@ pbdb_250308_carn_raw_dtf <- read.csv(here("data",
                                           "raw",
                                           "pbdb_occs_2025-04-03_raw.csv"),
                                      skip = 19)
-
+NCmisc::list.functions.in.file(here("scripts",
+                                    "pbdb_occurrence_names.R"))
 # Preparação dos dados para a harmonização
-
-pbdb_colnames_vct <- c("pbdb_identified_id" = "identified_no",
-                       "pbdb_identified_rank" = "identified_rank",
-                       "pbdb_identified_name" = "identified_name",
-                       "pbdb_accepted_authority" = "accepted_attr",
-                       "pbdb_status_difference" = "difference",
-                       "pbdb_accepted_id" = "accepted_no",
-                       "pbdb_accepted_rank" = "accepted_rank",
-                       "pbdb_accepted_name" = "accepted_name",
-                       "pbdb_accepted_sist_phylum_name" = "phylum",
-                       "pbdb_accepted_sist_phylum_id" = "phylum_no",
-                       "pbdb_accepted_sist_class_name" = "class",
-                       "pbdb_accepted_sist_class_id" = "class_no",
-                       "pbdb_accepted_sist_order_name" = "order",
-                       "pbdb_accepted_sist_order_id" = "order_no",
-                       "pbdb_accepted_sist_family_name" = "family",
-                       "pbdb_accepted_sist_family_id" = "family_no",
-                       "pbdb_accepted_sist_genus_name" = "genus",
-                       "pbdb_accepted_sist_genus_id" = "genus_no",
-                       "pbdb_accepted_sist_subgenus_id" = "subgenus_no",
-                       "pbdb_accepted_sist_subgenus_name" = "subgenus_name",
-                       "pbdb_info_subgenus_resolution" = "subgenus_reso",
-                       "pbdb_info_primary_resolution" = "primary_reso",
-                       "pbdb_info_species_resolution" = "species_reso")
 
 pbdb_occs_names <- pbdb_250308_carn_raw_dtf %>%
   select(identified_name,
@@ -76,8 +50,7 @@ pbdb_occs_names <- pbdb_250308_carn_raw_dtf %>%
          accepted_no) %>%
   mutate(across(where(is.character),
                 ~na_if(., "")),
-         family_no = as.integer(na_if(family_no, "NF"))) %>%
-  rename(all_of(pbdb_colnames_vct))
+         family_no = as.integer(na_if(family_no, "NF")))
 
 # Salvando o arquivo
 write.csv(pbdb_occs_names,
